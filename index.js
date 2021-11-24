c2608ebb303112ffe8f6a3f4747e4b6a7117337b
V1Bro <85158342+rasil859@users.noreply.github.com>
2021-11-24 09:19:54 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const settings = require("./settings.json");
+const express =  require("express");
+const port = process.env.PORT || 8000
+const app = express();
+const server = http.createServer(app);
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render('index', { bot: settings.website })
+})
+
+app.get("/commands", (req, res) => {
+    res.render("commands", {bot: settings.website, commands: settings.commands })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", { bot: settings.website })
+})
+
+server.listen(port, () => {
+    console.log({
+        message: `Server listening on http://localhost:${port}`,
+        badge: true
+    })
+})
+
+
